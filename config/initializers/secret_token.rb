440899fa8a11a50c75a0581ddc304013fb52e95a
James Sullivan <james@ip-172-31-45-37.us-west-2.compute.internal>
2013-06-24 03:48:34 +0000

    Initializing rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QueryableIrcStats::Application.config.secret_token = 'a37400361647bb0c539de87c05ff6d340923bc1a74729a74028fb007fcc93140df279a7702c298f4ea689ad773d76521e5006e4ec64a62c3c95b4d4d7e306e66'
