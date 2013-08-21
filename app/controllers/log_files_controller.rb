90179ab430bfd7246990180bed0489fa30e48a96
James <James@TheFlightlessGeek.co.nz>
2013-08-21 21:55:36 +1200

    added a basic file upload so users can push their log files up

--- app/controllers/log_files_controller.rb
+++ app/controllers/log_files_controller.rb
@@ -77,7 +77,14 @@ class LogFilesController < ApplicationController
     end
   end
   
+  # POST /log_files/upload
+  # Take the given file and save it to disc.
+  # Next step is then to process it and save each line to a database
   def upload
-    
+    uploaded_io = params[:log_file]
+
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+      file.write(uploaded_io.read)
+    end
   end
 end
