1dc8c432aa58771ce6e9c3ac3b420c61523b0d47
James <James@TheFlightlessGeek.co.nz>
2013-08-27 23:01:32 +1200

    Added basic recording of users. Regex is a bit greedy so needs polishing and duplicate detection needs to be added.

--- app/models/log_file_processor.rb
+++ app/models/log_file_processor.rb
@@ -1,35 +1,29 @@
 class LogFileProcessor
 
   def processLogFile(filenameOfLogFile)
-    arrayOfLines = Array.new
-    
-    IO.foreach(filenameOfLogFile) do |line|
-      arrayOfLines << line
-    end
-    
     day = 1
     month = "jan"
     year = 2013
 
-    arrayOfLines.each do |l|
+    IO.foreach(filenameOfLogFile) do |line|
     
-      if l.match(/^$/)
+      if line.match(/^$/)
         next
       end
     
-      if l.match(/^---/) then
-        l.match(/(\w\w\w) (\d\d)(?: \d\d:\d\d:\d\d)? (\d\d\d\d)/)
+      if line.match(/^---/) then
+        line.match(/(\w\w\w) (\d\d)(?: \d\d:\d\d:\d\d)? (\d\d\d\d)/)
         day = $2
         month = $1
         year = $3
         next
       end
       
-      if l.match(/^\d\d:\d\d -!-/) then
-        next        
+      if line.match(/^\d\d:\d\d -!-/) then
+        processSystemMessage(line)
       end
     
-      l.match(/^(\d\d):(\d\d) <.(.*)> (.*)/)
+      line.match(/^(\d\d):(\d\d) <.(.*)> (.*)/)
       newMessage = Message.new
 
       newMessage.nick = $3
@@ -41,4 +35,15 @@ class LogFileProcessor
     end
   end
   
+  def processSystemMessage(line)
+    line.match(/^\d\d:\d\d -!- (.*) \[(.*)@(.*)\]/)
+    
+    newNick = Nick.new
+    
+    newNick.name = $1
+    newNick.username = $2
+    newNick.hostname = $3
+    
+    newNick.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130624103800) do
+ActiveRecord::Schema.define(:version => 20130827101723) do
 
   create_table "messages", :force => true do |t|
     t.string   "nick"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(:version => 20130624103800) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.datetime "said_at"
+  end
+
+  create_table "nicks", :force => true do |t|
+    t.text     "name"
+    t.text     "hostname"
+    t.text     "username"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
