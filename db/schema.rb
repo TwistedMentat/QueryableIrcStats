26010532f290fd11f0bb49486f80e47939bc751a
James <James@TheFlightlessGeek.co.nz>
2014-04-07 21:31:32 +1200

    Added a property to identify admins.

--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,20 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140331063037) do
+ActiveRecord::Schema.define(version: 20140407092733) do
 
-  create_table "hostnames", :force => true do |t|
+  create_table "hostnames", force: true do |t|
     t.string   "domain_name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "messages", :force => true do |t|
+  create_table "messages", force: true do |t|
     t.text     "message"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "said_at"
     t.text     "action"
     t.integer  "nick_id"
@@ -30,34 +30,35 @@ ActiveRecord::Schema.define(:version => 20140331063037) do
     t.integer  "minute"
   end
 
-  create_table "nick_hostnames", :force => true do |t|
-    t.integer "nick_id",     :null => false
-    t.integer "hostname_id", :null => false
+  create_table "nick_hostnames", force: true do |t|
+    t.integer "nick_id",     null: false
+    t.integer "hostname_id", null: false
   end
 
-  create_table "nicks", :force => true do |t|
+  create_table "nicks", force: true do |t|
     t.text     "name"
     t.text     "username"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
