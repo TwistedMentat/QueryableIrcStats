7d2f369e3f1de802c99febb6aee147954f5e3ade
James <James@TheFlightlessGeek.co.nz>
2014-04-22 20:51:38 +1200

    Switched to using postgres instead of SQLite for the DB as I wanted to try something else as well as all the small bits of text we've got a full stored proc supporting environment could prove to be useful for the deduplication stuff.

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,19 @@
 
 ActiveRecord::Schema.define(version: 20140407092733) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hostnames", force: true do |t|
     t.string   "domain_name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "messages", force: true do |t|
     t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "said_at"
     t.text     "action"
     t.integer  "nick_id"
@@ -38,8 +41,8 @@ ActiveRecord::Schema.define(version: 20140407092733) do
   create_table "nicks", force: true do |t|
     t.text     "name"
     t.text     "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
@@ -53,12 +56,12 @@ ActiveRecord::Schema.define(version: 20140407092733) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
