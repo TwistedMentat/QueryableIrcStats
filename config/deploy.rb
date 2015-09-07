798d132b65a03e6a317cff5990e9fba36bccfcfd
James Sullivan <James@TheFlightlessGeek.co.nz>
2015-09-07 20:24:21 +1200

    Seems to be some suggestion of there being something odd in the ssh stuff and the error that I'm getting is happening on git:check. Swapping out the SSH URI for the SSL one.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'QueryableIrcStats'
-set :repo_url, 'git@github.com:TwistedMentat/QueryableIrcStats.git'
+set :repo_url, 'https://github.com/TwistedMentat/QueryableIrcStats.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
